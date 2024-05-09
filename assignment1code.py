8acfc5d00b49f5aabdcf0dbc89a2e96be05aa2ab
Destin Saba <destinsaba9@gmail.com>
2024-05-09 09:15:00 -0600

    Initial commit

---
+++ assignment1code.py
@@ -0,0 +1,10 @@
+# ENSF 692 Spring 2024
+# Assignment 1 Starter Code
+# Author: Destin Saba
+
+studentName = "Destin Saba"  # Change the contents of this variable to your name (leave the quotation marks).
+
+print("Hello, World!")
+print("My name is " + studentName + " and I am a student in ENSF 692.")
+
+print("My best skill is my ability to pick up and play new sports.")
